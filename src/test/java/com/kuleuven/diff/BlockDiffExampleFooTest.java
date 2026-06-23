4bcc2fda5a3501da99a9e24c3cce3c7f9b265d7e
yoran.mertens <yoran.mertens@datacamp.com>
2026-06-23 23:38:16 +0200

    chore: generate test suite

---
+++ src/test/java/com/kuleuven/diff/BlockDiffExampleFooTest.java
@@ -0,0 +1,129 @@
+package com.kuleuven.diff;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("GeneratedTestSuite")
+public class BlockDiffExampleFooTest {
+
+    @BeforeEach
+    void setUp() {
+        // Initialize tests
+
+    }
+
+    @Test
+    @Tag("11da6793c5249d0c0d4428416f583455dee1e1d8bd3f6c4985d580e1760fc139")
+    @Tag("3b7b9bce2e5b01ec1825a2a94b4cb7fd2b61737e46cc89ca7d750bc6343a860a")
+    @Tag("8a80afe69991e414f34cf30d30b3c57309826c617e4fc01758d3be3396cd5eb5")
+    public void test0() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(1073741824);
+
+        // perform assertions
+        assertEquals(-2147483648, result);
+
+    }
+
+    @Test
+    @Tag("05b6caf2667aa30774c04603c90134f1d0adece80d57abc9693fb398a730180c")
+    @Tag("11da6793c5249d0c0d4428416f583455dee1e1d8bd3f6c4985d580e1760fc139")
+    @Tag("14c1f78f6094efff30230d8dc39077d840a2b7e084e899f4e00cbd517e1df4a1")
+    @Tag("515218a740c2c1b6b2fbedee122bf4432ac6833c47db8d96bb8cba186138a8e8")
+    @Tag("8a80afe69991e414f34cf30d30b3c57309826c617e4fc01758d3be3396cd5eb5")
+    @Tag("d6fa1c2211352c07f89817ae8e91822aa4a7646d3ea3af8d925a79b0e3b91b91")
+    @Tag("e21ee22b96fa94716da2618db0987d86f7dd840a9fcac2eb753f74f499cda846")
+    public void test1() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(2);
+
+        // perform assertions
+        assertEquals(5, result);
+
+    }
+
+    @Test
+    @Tag("05b6caf2667aa30774c04603c90134f1d0adece80d57abc9693fb398a730180c")
+    @Tag("11da6793c5249d0c0d4428416f583455dee1e1d8bd3f6c4985d580e1760fc139")
+    @Tag("14c1f78f6094efff30230d8dc39077d840a2b7e084e899f4e00cbd517e1df4a1")
+    @Tag("515218a740c2c1b6b2fbedee122bf4432ac6833c47db8d96bb8cba186138a8e8")
+    @Tag("8a80afe69991e414f34cf30d30b3c57309826c617e4fc01758d3be3396cd5eb5")
+    @Tag("ccb43006bc91fca1b4eb1f9481c5c7cc4e249e3c73ae7ade4bb3ede695792744")
+    @Tag("e21ee22b96fa94716da2618db0987d86f7dd840a9fcac2eb753f74f499cda846")
+    public void test2() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(5);
+
+        // perform assertions
+        assertEquals(13, result);
+
+    }
+
+    @Test
+    @Tag("05b6caf2667aa30774c04603c90134f1d0adece80d57abc9693fb398a730180c")
+    @Tag("11da6793c5249d0c0d4428416f583455dee1e1d8bd3f6c4985d580e1760fc139")
+    @Tag("14a684085e4ee90037b5ca1fb6eaf70984999f9020fee5dbb75fecdeda02b0a9")
+    @Tag("467f8a09e1f1fe7a8ea25cbbab04cdd3d0fc759aba1c406353a495025cf32b4c")
+    @Tag("515218a740c2c1b6b2fbedee122bf4432ac6833c47db8d96bb8cba186138a8e8")
+    @Tag("8a80afe69991e414f34cf30d30b3c57309826c617e4fc01758d3be3396cd5eb5")
+    @Tag("d9ae1ee58990f5354a61da587fff3e7f6901d6d1af3a5da986cd7d1864e5d4a6")
+    public void test3() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(6);
+
+        // perform assertions
+        assertEquals(5, result);
+
+    }
+
+    @Test
+    @Tag("11da6793c5249d0c0d4428416f583455dee1e1d8bd3f6c4985d580e1760fc139")
+    @Tag("14c1f78f6094efff30230d8dc39077d840a2b7e084e899f4e00cbd517e1df4a1")
+    @Tag("24a5d19474b4e4eb4c2015847bad73b40b9735c1845828de65b48c9d59c8f594")
+    @Tag("3637a1516d13cb7e6e2546c0534396a22396d95f8c22fb08352ec9f73de45891")
+    @Tag("515218a740c2c1b6b2fbedee122bf4432ac6833c47db8d96bb8cba186138a8e8")
+    @Tag("8a80afe69991e414f34cf30d30b3c57309826c617e4fc01758d3be3396cd5eb5")
+    public void test4() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(60);
+
+        // perform assertions
+        assertEquals(118, result);
+
+    }
+
+    @Test
+    @Tag("11da6793c5249d0c0d4428416f583455dee1e1d8bd3f6c4985d580e1760fc139")
+    @Tag("c3fe3ea6307fb490717bde8327028ec842dba784774f9acdba2330a7830e5af5")
+    public void test5() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(0);
+
+        // perform assertions
+        assertEquals(0, result);
+
+    }
+
+    @Test
+    @Tag("14c1f78f6094efff30230d8dc39077d840a2b7e084e899f4e00cbd517e1df4a1")
+    @Tag("328a912f1f7a83ceccd4fd739b8b5c9673e833d1af9e917843f16c6424a77c5f")
+    @Tag("dd37e6627fda9e84e7c1f42da12ee15a7158150dcf0680027ae5774ac9a50e4f")
+    public void test6() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(-20);
+
+        // perform assertions
+        assertEquals(-45, result);
+
+    }
+
+    @Test
+    @Tag("384c6cdb708520d930f74454cfff5b6bd778fea4966f59ee2e3ba4cc92c71c9f")
+    @Tag("467f8a09e1f1fe7a8ea25cbbab04cdd3d0fc759aba1c406353a495025cf32b4c")
+    @Tag("dd37e6627fda9e84e7c1f42da12ee15a7158150dcf0680027ae5774ac9a50e4f")
+    public void test7() {
+        int result = com.kuleuven.diff.BlockDiffExample.foo(-2147483648);
+
+        // perform assertions
+        assertEquals(2147483647, result);
+
+    }
+
+}
+
